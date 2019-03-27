3b49cc79f728dda750d6cd2451d7b7e70c21b01c
Javier Gonzalez <xaviergonz@gmail.com>
2019-03-28 00:27:17 +0100

    some optimizations

--- src/shared/useMobxObserver.ts
+++ src/shared/useMobxObserver.ts
@@ -1,23 +1,19 @@
 import { getDependencyTree, Reaction } from "mobx"
-import { isUsingStaticRendering, useForceUpdate } from "mobx-react-lite"
-import { useDebugValue, useLayoutEffect, useRef } from "react"
+import { isUsingStaticRendering } from "mobx-react-lite"
+import { useDebugValue, useLayoutEffect, useRef, useState } from "react"
 
 export function useMobxObserver<T>(fn: () => T, baseComponentName: string = "observed"): T {
     if (isUsingStaticRendering()) {
         return fn()
     }
 
+    const [, setTick] = useState(0)
+
     const reaction = useRef<Reaction | null>(null)
-    const disposeReaction = () => {
-        if (reaction.current) {
-            reaction.current.dispose()
-            reaction.current = null
-        }
-    }
 
     useDebugValue(reaction, printDebugValue)
 
-    useLayoutEffect(() => disposeReaction, [])
+    useLayoutEffect(() => () => disposeReaction(reaction), [])
 
     // render the original component, but have the
     // reaction track the observables, so that rendering
@@ -25,13 +21,10 @@ export function useMobxObserver<T>(fn: () => T, baseComponentName: string = "obs
 
     // we use a new reaction to ensure we don't react to observables set in the render phase
     // this is different from how mobx-react-lite does, where it reuses the reaction
-    disposeReaction()
-    const forceUpdate = useForceUpdate()
-    if (!reaction.current) {
-        reaction.current = new Reaction(`observer(${baseComponentName})`, () => {
-            forceUpdate()
-        })
-    }
+    disposeReaction(reaction)
+    reaction.current = new Reaction(`observer(${baseComponentName})`, () => {
+        setTick(t => t + 1)
+    })
 
     let rendering!: T
     let exception
@@ -54,4 +47,11 @@ export function printDebugValue(v: React.MutableRefObject<Reaction | null>) {
         return "<unknown>"
     }
     return getDependencyTree(v.current)
+}
+
+function disposeReaction(reaction: React.MutableRefObject<Reaction | null>) {
+    if (reaction.current) {
+        reaction.current.dispose()
+        reaction.current = null
+    }
 }
