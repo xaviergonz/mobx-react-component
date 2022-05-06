d7da24db307716efddb290b4fd4027cc776e4bdc
xaviergonz <xaviergonz@gmail.com>
2022-05-06 23:54:26 +0200

    update deps

--- packages/mobx-react-component/test/strictAndConcurrentMode.test.tsx
+++ packages/mobx-react-component/test/strictAndConcurrentMode.test.tsx
@@ -3,6 +3,7 @@ import mockConsole from "jest-mock-console"
 import * as mobx from "mobx"
 import * as React from "react"
 import { mobxObserver } from "../src"
+import { useMobxObserver } from "../src/shared/useMobxObserver"
 import { globalSetup } from "./utils"
 
 globalSetup()
@@ -41,4 +42,29 @@ test("uncommitted observing components should not attempt state changes", () => 
     } finally {
         restoreConsole()
     }
+})
+
+test(`observable changes before first commit are not lost`, async () => {
+    const store = mobx.observable({ value: "initial" })
+
+    const TestComponent = () =>
+        useMobxObserver(() => {
+            const res = <div>{store.value}</div>
+            // Change our observable. This is happening between the initial render of
+            // our component and its initial commit, so it isn't fully mounted yet.
+            // We want to ensure that the change isn't lost.
+            store.value = "changed"
+            return res
+        })
+
+    const rootNode = document.createElement("div")
+    document.body.appendChild(rootNode)
+
+    const rendering = render(
+        <React.StrictMode>
+            <TestComponent />
+        </React.StrictMode>
+    )
+
+    expect(rendering.baseElement.textContent).toBe("changed")
 })
